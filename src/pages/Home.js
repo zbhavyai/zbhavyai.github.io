07ab0411abb686caeb3437a9389b9298ee51acb1
Bhavyai Gupta <zbhavyai@gmail.com>
2023-09-17 23:46:30 -0600

    update about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from '../components/Header';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header activeNav='about' />
-      Home page
+      <Container className='d-flex justify-content-center align-items-center' style={{ minHeight: '80vh' }}>
+        <div>
+          <h1>Bhavyai Gupta</h1>
+          <h2>Software Developer</h2>
+        </div>
+      </Container>
     </React.Fragment>
   );
 };
