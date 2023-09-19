5dcb0852b44a11b492de69324f90c7cb1bfbd1e9
Bhavyai Gupta <zbhavyai@gmail.com>
2023-09-18 19:42:05 -0600

    add navItem

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,15 +12,21 @@ const Header = ({ activeNav }) => {
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
           <Nav className='ml-auto monoFont' variant='pills' activeKey={activeNav}>
-            <Nav.Link eventKey='about' href='/about' className='px-3'>
-              About
-            </Nav.Link>
-            <Nav.Link eventKey='projects' href='/projects' className='px-3'>
-              Projects
-            </Nav.Link>
-            <Nav.Link eventKey='resume' href='/resume' className='px-3'>
-              Resume
-            </Nav.Link>
+            <Nav.Item>
+              <Nav.Link eventKey='about' href='/about' className='px-3'>
+                About
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='projects' href='/projects' className='px-3'>
+                Projects
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='resume' href='/resume' className='px-3'>
+                Resume
+              </Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
