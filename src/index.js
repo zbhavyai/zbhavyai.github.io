55f45d273c7d2e506b70c3fa7816dec29fbaa288
Bhavyai Gupta <zbhavyai@gmail.com>
2024-05-05 15:47:05 -0600

    combine index and app component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { default as React } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import "./styles/main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
