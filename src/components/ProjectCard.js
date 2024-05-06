8c221c25cfd8ab06d765a7b49b1b1d6f1b641c27
Bhavyai Gupta <zbhavyai@gmail.com>
2024-05-05 23:26:43 -0600

    rework project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,58 +1,49 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Badge, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import React from "react";
+import { Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ props }) => {
-  let brokenCardImage = `${process.env.PUBLIC_URL + '/logo512.png'}`;
+  let brokenCardImage = `${process.env.PUBLIC_URL + "/logo512.png"}`;
 
   return (
-    <Card className='shadow rounded monoFontFira card-project'>
-      <Card.Img
-        variant='top'
-        src={props['card_image']}
-        alt={props['card_image']}
-        onError={(e) => {
-          e.target.src = brokenCardImage;
-        }}
-        height={'200px'}
-        className='p-5'
-        style={{ objectFit: 'contain', backgroundColor: '#e8e9ea' }}
-      />
-      <Card.Body className='p-0'>
-        <Card.Title className='py-1 px-2 bg-secondary text-light d-flex justify-content-between'>
-          {props['name']}
-          <Link to={props['repository_link']} target='_blank' rel='noopener noreferrer' className='text-light'>
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-        </Card.Title>
-        <Card.Text
-          className='px-3 mb-2'
-          style={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            WebkitBoxOrient: 'vertical',
-            display: '-webkit-box',
-            WebkitLineClamp: 3,
-            lineClamp: 3,
-            height: '4.5em',
-          }}
-        >
-          {props['short_description']}
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer className='text-end' style={{ fontSize: '0.85em' }}>
-        {props['tech_stack']?.map((language, index) => {
-          return (
-            <Badge key={index} pill className='bg-secondary ms-1'>
-              {language}
-            </Badge>
-          );
-        })}
-      </Card.Footer>
-    </Card>
+    <Link className="zbhavyai-project-link" to={props["repository_link"]} target="_blank" rel="noopener noreferrer">
+      <Card className="zbhavyai-project-card shadow" height={"100px"}>
+        <Card.Body className="p-0 d-flex flex-row">
+          <Image
+            className="zbhavyai-project-card-image p-3"
+            src={props["card_image"]}
+            alt={props["card_image"]}
+            roundedCircle
+            onError={(e) => {
+              e.target.src = brokenCardImage;
+            }}
+          />
+          <Card.Text className="py-3">
+            <h6 className="zbhavyai-project-card-title px-3">{props["name"]}</h6>
+            <div className="zbhavyai-project-card-text px-3">{props["summary"]}</div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
+
+ProjectCard.propTypes = {
+  props: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    repository_link: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    card_image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+// ProjectCard.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   summary: PropTypes.string.isRequired,
+//   card_image: PropTypes.string.isRequired,
+//   repository_link: PropTypes.string.isRequired,
+// };
 
 export default ProjectCard;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import Header from '../components/Header';
-import ProjectCard from '../components/ProjectCard';
-import projectList from '../data/projects.json';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Header from "../components/Header";
+import ProjectCard from "../components/ProjectCard";
+import projectList from "../data/projects.json";
 
 const Projects = () => {
   return (
     <React.Fragment>
-      <Header activeNav='projects' />
-      <Container>
-        <Row id='container-projects' xs={1} sm={2} md={2} lg={4} className='g-4 m-4'>
+      <Header activeNav="projects" />
+      <Container className="pt-4">
+        <Row xs={1} sm={2} md={3} lg={3} className="g-4 m-4">
           {projectList?.map((project, index) => {
             return (
-              <Col key={index}>
-                <ProjectCard key={project['name']} props={project} />
+              <Col className="p-2" key={index}>
+                <ProjectCard key={project["name"]} props={project} />
               </Col>
             );
           })}
