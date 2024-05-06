e4fe729c0ca5b054e7fdcf6ea7348c68b6fb0c8c
Bhavyai Gupta <zbhavyai@gmail.com>
2024-05-05 21:00:31 -0600

    add full-stack

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
           <Col md={12}>
             <h1 className="zbhavyai-h1-about fira-mono-bold py-2">Hi! I am Bhavyai.</h1>
             <h2 className="fira-mono-regular">
-              I do software development
+              I do full-stack software development
               <br />
               for fun and profit.
             </h2>
