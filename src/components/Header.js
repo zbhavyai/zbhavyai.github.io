6750277118a98ba49583f3cd3490cb95d89dbc3f
Bhavyai Gupta <zbhavyai@gmail.com>
2024-05-05 19:42:43 -0600

    rework header and reorg css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = ({ activeNav }) => {
   return (
-    <Navbar bg='dark' data-bs-theme='dark' collapseOnSelect expand='sm' className='bg-body-tertiary navbar-dark justify-content-between'>
+    <Navbar className="zbhavyai-navbar navbar-dark bg-body-tertiary justify-content-between" bg="dark" expand="sm" collapseOnSelect>
       <Container>
-        <Navbar.Brand href='/' className='fw-bold'>
-          भव्य गुप्ता
+        <Navbar.Brand href="/" className="zbhavyai-brand fira-mono-bold fw-bold">
+          Bhavyai Gupta
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
-          <Nav className='ml-auto monoFontFira' variant='pills' activeKey={activeNav}>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto" variant="pills" activeKey={activeNav}>
             <Nav.Item>
-              <Nav.Link eventKey='about' href='/about' className='px-3'>
+              <Nav.Link eventKey="about" href="/" className="px-3">
                 About
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey='projects' href='/projects' className='px-3'>
+              <Nav.Link eventKey="projects" href="/projects" className="px-3">
                 Projects
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey='resume' href='/resume' className='px-3'>
+              <Nav.Link eventKey="resume" href="/resume" className="px-3">
                 Resume
               </Nav.Link>
             </Nav.Item>
