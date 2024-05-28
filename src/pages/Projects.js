71ec04bf15d88612a7a8bfad45670366082040f7
Bhavyai Gupta <zbhavyai@gmail.com>
2024-05-27 20:26:31 -0600

    update project card spacing

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,10 +9,10 @@ const Projects = () => {
     <React.Fragment>
       <Header activeNav="projects" />
       <Container className="my-5">
-        <Row xs={1} sm={1} md={2} lg={3} className="g-4 m-4">
+        <Row xs={1} sm={1} md={2} lg={3} className="g-4 m-md-4 m-lg-4 m-1">
           {projectList?.map((project, index) => {
             return (
-              <Col className="p-0 p-md-2 p-lg-2" key={index}>
+              <Col className="p-0 px-md-3 px-lg-3" key={index}>
                 <ProjectCard key={project["name"]} props={project} />
               </Col>
             );
