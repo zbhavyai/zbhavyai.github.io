315a304ee1991103295f2f9529dedc7599c61302
Bhavyai Gupta <zbhavyai@gmail.com>
2024-07-02 13:31:46 -0600

    separate out profiles data to a json

--- src/components/SocialStrip.js
+++ src/components/SocialStrip.js
@@ -1,34 +1,35 @@
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord, faFacebook, faGithub, faGoogle, faHackerrank, faLinkedinIn, faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ProfileList from "../data/profiles.json";
+
+const iconMap = {
+  faDiscord,
+  faEnvelope,
+  faFacebook,
+  faGithub,
+  faGoogle,
+  faHackerrank,
+  faLinkedinIn,
+  faXTwitter,
+  faYoutube,
+};
 
 const SocialStrip = () => {
   return (
     <Container className="zbhavyai-container-social px-0 py-3">
-      <Link to="https://www.github.com/zbhavyai" target="_blank" rel="noopener noreferrer" className="github social">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </Link>
-      {/* <Link to="https://www.youtube.com/@zbhavyai" target="_blank" rel="noopener noreferrer" className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </Link> */}
-      {/* <Link to="https://www.facebook.com/zbhavyai" target="_blank" rel="noopener noreferrer" className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </Link> */}
-      {/* <Link to="https://www.twitter.com/zbhavyai" target="_blank" rel="noopener noreferrer" className="twitter social">
-        <FontAwesomeIcon icon={faXTwitter} size="2x" />
-      </Link> */}
-      {/* <Link to="https://discord.com/users/zbhavyai" target="_blank" rel="noopener noreferrer" className="twitter social">
-        <FontAwesomeIcon icon={faDiscord} size="2x" />
-      </Link> */}
-      <Link to="https://www.linkedin.com/in/zbhavyai" target="_blank" rel="noopener noreferrer" className="linkedin social">
-        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-      </Link>
-      <Link to="mailto:zbhavyai@gmail.com" target="_blank" rel="noopener noreferrer" className="email social">
-        <FontAwesomeIcon icon={faEnvelope} size="2x" />
-      </Link>
+      {ProfileList?.filter((profile) => profile["show"]).map((profile, index) => {
+        const IconComponent = iconMap[profile["faIcon"]];
+
+        return (
+          <Link key={profile["name"]} to={profile["link"]} target="_blank" rel="noopener noreferrer" className="social">
+            <FontAwesomeIcon icon={IconComponent} size="2x" />
+          </Link>
+        );
+      })}
     </Container>
   );
 };
