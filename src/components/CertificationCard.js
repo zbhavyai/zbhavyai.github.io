c80f16bcd6fe9fe1a0036331c8e57b4c91034c26
Bhavyai Gupta <zbhavyai@gmail.com>
2024-12-01 22:13:55 -0700

    add certification data and update its components

--- src/components/CertificationCard.js
+++ src/components/CertificationCard.js
@@ -1,17 +1,25 @@
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const CertificationCard = ({ props }) => {
   return (
     <Card as="article" className="zbhavyai-certification-card">
       <Card.Body as="section" className="d-flex justify-content-between">
         <div>
-          <Card.Title>{props["certification"]}</Card.Title>
+          <Card.Title>
+            <Link className="zbhavyai-certificate-link" to={props["credential_url"]} target="_blank" rel="noopener noreferrer">
+              {props["certification"]}
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="zbhavyai-external-link-icon" />
+            </Link>
+          </Card.Title>
           <Card.Text>{props["organization"]}</Card.Text>
         </div>
         <Card.Text as="time">
-          {props["certified_on"]} - {props["valid_till"]}
+          {props["issue_date"]} - {props["expiry_date"]}
         </Card.Text>
       </Card.Body>
     </Card>
@@ -22,8 +30,8 @@ CertificationCard.propTypes = {
   props: PropTypes.shape({
     certification: PropTypes.string.isRequired,
     organization: PropTypes.string.isRequired,
-    certified_on: PropTypes.string.isRequired,
-    valid_till: PropTypes.string.isRequired,
+    issue_date: PropTypes.string.isRequired,
+    expiry_date: PropTypes.string.isRequired,
   }).isRequired,
 };
 
