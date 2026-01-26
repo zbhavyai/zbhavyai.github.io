d65c413c8e056737a42ff0fc10459f0ef603b7e0
Bhavyai Gupta <zbhavyai@gmail.com>
2026-01-25 19:33:12 -0700

    add my image on the about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 import Header from "../components/Header";
 import SocialStrip from "../components/SocialStrip";
 
@@ -8,8 +9,11 @@ export default function Home() {
     <React.Fragment>
       <Header activeNav="about" />
       <Container className="zbhavyai-container-about d-flex justify-content-center align-items-center">
-        <Row>
-          <Col md={12} className="zbhavyai-col-about p-lg-2 p-4">
+        <Row className="align-items-center">
+          <Col md={4} className="d-flex justify-content-center my-3">
+            <Image src="/logo512.png" alt="Bhavyai Gupta" roundedCircle className="zbhavyai-logo" />
+          </Col>
+          <Col md={8} className="zbhavyai-col-about px-5 my-3">
             <h1 className="zbhavyai-h1-about fira-mono-bold py-2">Hi, I'm Bhavyai</h1>
             <h2 className="fira-mono-regular">
               Developer
